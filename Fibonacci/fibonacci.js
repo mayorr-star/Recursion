9b48971a23fb3c93ed7a61afcc2a4ef6883405b7
mayorr-star <immaboat11@gmail.com>
2024-06-01 15:02:02 +0000

    Solved recursively

--- Fibonacci/fibonacci.js
+++ Fibonacci/fibonacci.js
@@ -1,25 +1,28 @@
 function fibs(num) {
-    let fibsSeq = [0, 1];
-    if (num === 0) return [];
-    else if (num === 1) return [0];
-    else {
-        for (let i = 3; i <= num; i++) {
-            let nextNum = fibsSeq[fibsSeq.length- 1] + fibsSeq[fibsSeq.length- 2];
-            fibsSeq.push(nextNum);
-        }
-        return fibsSeq;
+  let fibsSeq = [0, 1];
+  if (num === 0) return [];
+  else if (num === 1) return [0];
+  else {
+    for (let i = 3; i <= num; i++) {
+      let nextNum = fibsSeq[fibsSeq.length - 1] + fibsSeq[fibsSeq.length - 2];
+      fibsSeq.push(nextNum);
     }
+    return fibsSeq;
+  }
 }
 
-
-console.log(fibs(0))
-console.log(fibs(1))
-console.log(fibs(2))
-console.log(fibs(3))
-console.log(fibs(4))
-console.log(fibs(5))
-console.log(fibs(6))
-console.log(fibs(7))
-console.log(fibs(8))
-console.log(fibs(9))
-console.log(fibs(10))
+function fibRec(num) {
+  if (num === 1) return [0];
+  else if (num === 2) return [0, 1]
+  else if (num === 0) return [];
+  else {
+    let fibSeq = fibRec(num - 1)
+    fibSeq.push(fibSeq[fibSeq.length - 2] + fibSeq[fibSeq.length - 1])
+    return fibSeq;
+  }
+}
+console.log(fibRec(0))
+console.log(fibRec(1))
+console.log(fibRec(2))
+console.log(fibRec(8))
+console.log(fibRec(10))
