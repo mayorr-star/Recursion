d2200f19ab5060175a4beacbedfabaf941270fa0
mayorr-star <immaboat11@gmail.com>
2024-06-02 05:50:37 +0000

    modified

--- Fibonacci/fibonacci.js
+++ Fibonacci/fibonacci.js
@@ -1,28 +1,37 @@
 function fibs(num) {
-  let fibsSeq = [0, 1];
-  if (num === 0) return [];
-  else if (num === 1) return [0];
-  else {
-    for (let i = 3; i <= num; i++) {
-      let nextNum = fibsSeq[fibsSeq.length - 1] + fibsSeq[fibsSeq.length - 2];
-      fibsSeq.push(nextNum);
-    }
-    return fibsSeq;
+    if (num === 0) return []
+    else if (num === 1) return [0];
+  let firstFibNum = 0;
+  let secondFibNum = 1;
+  let nextFibNum = null;
+  const fibSequence = [0, 1];
+  for (let i = 3; i <= num; i++) {
+    nextFibNum = firstFibNum + secondFibNum;
+    fibSequence.push(nextFibNum);
+    firstFibNum = secondFibNum;
+    secondFibNum = nextFibNum;
   }
+  return fibSequence;
 }
+console.log(fibs(0))
+console.log(fibs(1))
+console.log(fibs(2))
+console.log(fibs(5))
+console.log(fibs(8))
+console.log("Recursive")
+console.log(fibsRec(0))
+console.log(fibsRec(1))
+console.log(fibsRec(2))
+console.log(fibsRec(5))
+console.log(fibsRec(8))
 
-function fibRec(num) {
-  if (num === 1) return [0];
-  else if (num === 2) return [0, 1]
-  else if (num === 0) return [];
-  else {
-    let fibSeq = fibRec(num - 1)
-    fibSeq.push(fibSeq[fibSeq.length - 2] + fibSeq[fibSeq.length - 1])
-    return fibSeq;
-  }
+function fibsRec(num) {
+    if (num === 0) return []
+    else if (num === 1) return [0]
+    else if (num === 2) return [0, 1]
+    else {
+        let fibSequence = fibsRec(num - 1);
+        fibSequence.push(fibSequence[fibSequence.length -1] + fibSequence[fibSequence.length - 2]);
+        return fibSequence;
+    }
 }
-console.log(fibRec(0))
-console.log(fibRec(1))
-console.log(fibRec(2))
-console.log(fibRec(8))
-console.log(fibRec(10))
