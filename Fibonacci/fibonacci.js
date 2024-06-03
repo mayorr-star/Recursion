2f9d22fe32d6c47ddf28607a74b2a1c8f018bf1c
mayorr-star <immaboat11@gmail.com>
2024-06-03 19:27:02 +0000

    Removed the test cases

--- Fibonacci/fibonacci.js
+++ Fibonacci/fibonacci.js
@@ -13,17 +13,6 @@ function fibs(num) {
   }
   return fibSequence;
 }
-console.log(fibs(0))
-console.log(fibs(1))
-console.log(fibs(2))
-console.log(fibs(5))
-console.log(fibs(8))
-console.log("Recursive")
-console.log(fibsRec(0))
-console.log(fibsRec(1))
-console.log(fibsRec(2))
-console.log(fibsRec(5))
-console.log(fibsRec(8))
 
 function fibsRec(num) {
     if (num === 0) return []
