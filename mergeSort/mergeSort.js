dec6134009362a3661a6130f4b075cebf93f8129
mayorr-star <immaboat11@gmail.com>
2024-06-03 19:25:33 +0000

    Delcared a function to sort array recursively

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -1,13 +1,3 @@
-// Get the first Array
-// Save it to a variable called leftHalf
-// Get the second array
-// Save it to a variable called rightHalf
-// while both arrays are not empty
-// compare first elements
-// If leftHalf[0] < rightHalf[0], store in a new array and remove from leftHalf
-// Else store rightHalf[0] in the new array and remove from rightHalf
-// if one array array is empty skip while loop and merge with the new array
-
 function mergeArrays(leftHalf, rightHalf) {
     const mergedArray = [];
     while (leftHalf.length > 0 && rightHalf.length > 0) {
@@ -19,4 +9,12 @@ function mergeArrays(leftHalf, rightHalf) {
     }
     return mergedArray.concat(leftHalf, rightHalf)
 }
-console.log(mergeArrays([1, 3, 7], [4, 5, 6]))
+
+function mergeSort(fullArray) {
+    if (fullArray.length <= 1) return fullArray;
+    const midpoint = Math.floor(fullArray.length / 2);
+    const leftHalf = mergeSort(fullArray.slice(0, midpoint));
+    const rightHalf = mergeSort(fullArray.slice(midpoint));
+    return mergeArrays(leftHalf, rightHalf)
+}
+
